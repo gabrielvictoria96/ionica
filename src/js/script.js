9b4fa697f873758d78d465685c7a8506a569cbe7
Gabriel Victoria <81654814+gabrielvictoria96@users.noreply.github.com>
2021-11-16 18:57:50 -0300

    Add Responsive Navbar

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,21 @@ const activeClass = 'ativo';
 // MENU MOBILE
 const mobileButton = document.querySelector('.navbar-btn');
 const navLinks = document.querySelector('.navbar-links');
+const links = document.querySelectorAll('.navbar-links li a');
 
 mobileButton.addEventListener('click', () => {
    navLinks.classList.toggle(activeClass);
 });
+
+
+
+links.forEach((link) => {
+   link.addEventListener('click', () => {
+      navLinks.classList.remove(activeClass);
+   });
+});
+  
+   
+
+
+

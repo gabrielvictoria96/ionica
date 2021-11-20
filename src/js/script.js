041d41ccf49e6b5253d42a0d9032b9def6ea738d
Gabriel Victoria <81654814+gabrielvictoria96@users.noreply.github.com>
2021-11-19 21:16:21 -0300

    Building Funcionamento Page

--- src/js/script.js
+++ src/js/script.js
@@ -19,5 +19,26 @@ links.forEach((link) => {
   
    
 
+function initiTabNav() {
+   const tabMenu = document.querySelectorAll('.js-tabmenu li');
+   const tabContent = document.querySelectorAll('.js-tabcontent section');
+   if(tabMenu.length && tabContent.length) {
+      tabContent[0].classList.add(activeClass);   
+   
+   function activeTab(index) {
+      tabContent.forEach((section) => {
+         section.classList.remove(activeClass);
+      });
+      tabContent[index].classList.add(activeClass);
+   }
+
+   tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => {
+         activeTab(index);
+         });
+      });
+   };
+};
+initiTabNav();
 
 
